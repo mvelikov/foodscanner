804adb7dff82a0d0b12bc934eac41d8195372ad2
Mihail Velikov <mihailvelik@gmail.com>
2013-06-02 22:55:29 +0300

    Initial Commit

---
+++ Food Scanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Food Scanner
+//
+//  Created by mihata on 6/2/13.
+//  Copyright (c) 2013 Mihail Velikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
